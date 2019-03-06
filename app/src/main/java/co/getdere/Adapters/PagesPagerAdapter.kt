03051cc4953eb1fc549d214ef43a0c57bd9ff54c
Yanay Zabary <yanay.tsabary@gmail.com>
2019-03-07 05:29:31 +1100

    merging into devlopment and master

--- app/src/main/java/co/getdere/Adapters/PagesPagerAdapter.kt
+++ app/src/main/java/co/getdere/Adapters/PagesPagerAdapter.kt
@@ -5,7 +5,6 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.util.SparseArray
 import co.getdere.Fragments.BoardFragment
-import co.getdere.Fragments.CameraFragment
 import co.getdere.Fragments.FeedFragment
 import co.getdere.Fragments.ProfileFragment
 import java.lang.ref.WeakReference
@@ -16,13 +15,12 @@ class PagesPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter
 
     override fun getItem(p0: Int) : Fragment {
 
-        val actualPosition = p0 % 4
+        val actualPosition = p0 % 3
         when (actualPosition) {
 
             0 -> return FeedFragment.newInstance()
             1 -> return BoardFragment.newInstance()
             2 -> return ProfileFragment.newInstance()
-            3 -> return CameraFragment()
             else -> return FeedFragment.newInstance()
         }
     }
